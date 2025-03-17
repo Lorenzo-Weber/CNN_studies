8ec499aaaead8d040dd33277b2658b4f1f44129b
lorenzo weber <lcweber@inf.ufsm.br>
2025-03-17 12:45:39 -0300

    CNN build

--- cnn.py
+++ cnn.py
@@ -16,6 +16,7 @@ fmaps = conv_layer(images)
 
 pool_layer = tf.keras.layers.MaxPooling2D(pool_size=2, strides=2)
 
+# Basically gets the max within the filters instead of going through all the filters
 class DepthPool(tf.keras.layers.Layer):
     def __init__(self, pool_size,**kwargs ):
         super().__init__(**kwargs)
@@ -29,3 +30,28 @@ class DepthPool(tf.keras.layers.Layer):
         return tf.reduce_max(tf.reshape(inputs, new_shape), axis=-1)
 
 global_pool = tf.keras.layers.GlobalAvgPool2D()
+
+print(global_pool(images))
+
+# Creating a CNN
+
+from functools import partial
+
+DefaultConv2D = partial(tf.keras.layers.Conv2D, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal')
+
+model = tf.keras.Sequential([
+    DefaultConv2D(filters=64, kernel_size=7, input_shape=[28, 28, 1]),
+    tf.keras.layers.MaxPool2D(),
+    DefaultConv2D(filters=128),
+    DefaultConv2D(filters=128),
+    tf.keras.layers.MaxPool2D(),
+    DefaultConv2D(filters=256),
+    DefaultConv2D(filters=256),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=128, activation='relu', kernel_initializer='he_normal'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(units=64, activation='relu', kernel_initializer='he_normal'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(units=10, activation='softmax')
+])
