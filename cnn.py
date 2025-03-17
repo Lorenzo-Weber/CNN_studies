076db183fe1b8c6243b8cada79d4f60b2fa7c7b7
lorenzo weber <lcweber@inf.ufsm.br>
2025-03-17 11:54:02 -0300

    pooling layers

--- cnn.py
+++ cnn.py
@@ -1,3 +1,8 @@
+# In order to run the code you must have the following packages installed:
+# - scikit-learn (pip install scikit-learn)
+# - tensorflow (pip install tensorflow or pip install tensorflow[gpu])
+# - PIL (pip install --upgrade Pillow)
+
 from sklearn.datasets import load_sample_images
 import tensorflow as tf
 
@@ -9,4 +14,18 @@ images = tf.keras.layers.Rescaling(scale=1/255.0)(images)
 conv_layer = tf.keras.layers.Conv2D(filters=32, kernel_size=7, padding='same')
 fmaps = conv_layer(images)
 
-print(fmaps.shape)
+pool_layer = tf.keras.layers.MaxPooling2D(pool_size=2, strides=2)
+
+class DepthPool(tf.keras.layers.Layer):
+    def __init__(self, pool_size,**kwargs ):
+        super().__init__(**kwargs)
+        self.pool_size = pool_size
+
+    def call(self, inputs):
+        shape = tf.shape(inputs)
+        groups = shape[-1] // self.pool_size
+        new_shape = tf.concat([shape[:-1], [groups, self.pool_size]], axis=0)
+
+        return tf.reduce_max(tf.reshape(inputs, new_shape), axis=-1)
+
+global_pool = tf.keras.layers.GlobalAvgPool2D()
